303200a154cb52f9b4e38fc59756fdd18590a5d8
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-01-31 23:17:58 +0530

    Update NewTest1.java

--- src/test/java/com/edu/NewTest1.java
+++ src/test/java/com/edu/NewTest1.java
@@ -21,7 +21,7 @@ public class NewTest1 {
 	    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 	    driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.get("http://34.69.172.3:8080/sampleapp/");
+		driver.get("http://35.192.173.34:9080/sampleapp/");
   }
   
   @Test
